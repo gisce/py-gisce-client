a9daa1fd94a68c9b6269fd5ded066cd12b91af3f
Eduard Carrerars <ecarreras@gisce.net>
2024-02-02 12:04:43 +0100

    Bump to v0.6.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='GISCE',
     author_email='devel@gisce.net',
     url='https://github.com/gisce/py-gisce-client',
-    version='0.5.1',
+    version='0.6.0',
     license='General Public Licence 2',
     long_description='''py-gisce-client is a Python client to access the GISCE ERP''',
     provides=['pygisceclient'],
