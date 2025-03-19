c7072cf38fc5597e3170c72975ed43b10ad126d0
PSala <polsalalidon@gmail.com>
2025-03-19 14:14:45 +0100

    Bump to v0.9.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='GISCE',
     author_email='devel@gisce.net',
     url='https://github.com/gisce/py-gisce-client',
-    version='0.8.1',
+    version='0.9.0',
     license='General Public Licence 2',
     long_description='''py-gisce-client is a Python client to access the GISCE ERP''',
     provides=['pygisceclient'],
